390bd6f2dfe096e205efec9d7e52d77a95950cdb
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-06 08:22:41 +0200

    Create ParametersExtension.php

---
+++ src/Drago/Parameters/DI/ParametersExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Drago Parameters
+ * Package built on Nette Framework
+ */
+namespace Drago\Parameters;
+
+use Nette\DI\CompilerExtension;
+
+
+/**
+ * Register services.
+ * @package Drago\Parameters
+ */
+class ParametersExtension extends CompilerExtension
+{
+	public function loadConfiguration(): void
+	{
+		$builder = $this->getContainerBuilder();
+		$params  = $builder->parameters;
+		$builder
+			->addDefinition($this->prefix('Directory'))
+			->setFactory(Directory::class, [
+				$params['appDir'],
+				$params['wwwDir'],
+				$params['tempDir'],
+				$params['vendorDir'],
+			]);
+
+		$builder
+			->addDefinition($this->prefix('Environment'))
+			->setFactory(Environment::class, [$params['productionMode']]);
+	}
+}
